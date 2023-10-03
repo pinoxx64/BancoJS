b2b85da5d9d7bfe4cff7752189e7669c8b725407
David Trujillo Carrero <davitru60@gmail.com>
2023-10-03 20:52:32 +0200

    retirarDinero

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-function cargarDatos(){
+function cargarDatos() {
     /*document.getElementById('idPersona').value = persona.id
     document.getElementById('nombre').value = persona.nombre
     document.getElementById('apellido1').value = persona.apellido1
@@ -9,7 +9,83 @@ function cargarDatos(){
     menu = document.getElementById('menu').innerHTML
 }
 
-function cargarCabecera(dest){  
- document.getElementById(dest).innerHTML = '   <h1>BancoPuertollano</h1>    <ul>        <li><a href="index.html">Inicio</a></li>        <li><a href="infoCuenta.html">Informaci&#243;n Cuenta</a></li>             <li><a href="tarjetas.html">Tarjetas</a></li>    </ul>' 
+function cargarCabecera(dest) {
+    document.getElementById(dest).innerHTML = '   <h1>BancoPuertollano</h1>    <ul>        <li><a href="index.html">Inicio</a></li>        <li><a href="infoCuenta.html">Informaci&#243;n Cuenta</a></li>             <li><a href="tarjetas.html">Tarjetas</a></li>    </ul>'
 }
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    class Cuenta {
+        constructor(iban, saldo) {
+            this.iban = iban
+            this.saldo = saldo
+        }
+
+        ingresar(cantidad) {
+            var haySuficiente=true
+
+            if(cantidad>0){
+                this.saldo -=cantidad
+                haySuficiente=true
+            }else{
+                haySuficiente=false
+            }
+
+            return haySuficiente
+        }
+
+        retirar(cantidad){
+            var haySuficiente=true
+            if(cantidad<this.saldo && cantidad>0){
+                this.saldo-=cantidad
+                haySuficiente=true
+            }else{
+                haySuficiente=false
+            }
+
+            return haySuficiente
+        }
+    }
+
+
+    var cuenta = new Cuenta("ES21 1465 0100 72 2030876293", 500)
+
+
+    var ibanInput = document.getElementById("iban")
+    var saldoInput = document.getElementById("saldo")
+    var btnRetirar= document.getElementById("btn-retirar")
+    var btnIngresar = document.getElementById("btn-ingresar")
+    var mensaje = document.getElementById("mensaje")
+
+
+    ibanInput.value = cuenta.iban
+    ibanInput.readOnly = true
+
+    saldoInput.value = cuenta.saldo
+    saldoInput.readOnly = true
+
+
+    btnRetirar.addEventListener("click",retirarSaldo)
+
+    function retirarSaldo(){
+        var saldoRetirar = document.getElementById("saldo-retirar")
+        var cantidad=parseFloat(saldoRetirar.value)
+        var esNumero=/\d/
+
+        if(cuenta.retirar(cantidad) && esNumero.test(cantidad)){
+            saldoInput.value=cuenta.saldo
+            mensaje.textContent="Dinero retirado correctamente: "+cantidad 
+
+        }else{
+            console.log("Algo va mal")
+        }
+
+    }
+
+})
+
+
+
+
 
