6825a17568c5602887f8800c6300f0c968563283
Raul GM <loscanos3raul@gmail.com>
2023-10-06 14:24:17 +0200

    final fde final

--- js/tarjetaUI.js
+++ js/tarjetaUI.js
@@ -1,14 +1,18 @@
 import {Tarjeta,Cuenta} from "./clases.js"
 
+
 document.addEventListener("DOMContentLoaded", function () {
     var boton=document.getElementById('guardar')
     boton.addEventListener('click', guardar)
+    var tarjetas = []
+    var cuenta = new Cuenta("ES21 1465 0100 72 2030876293", 500, tarjetas)
+    var tarjeta=[]
+
     function guardar() {
-        var tarjetas = []
-        var cuenta = new Cuenta("ES21 1465 0100 72 2030876293", 500, tarjetas)
+
         var num = document.getElementById('tarjeta').value
         var cvv = document.getElementById('cvv').value
-        var act = document.getElementById('act').value
+        var act = document.getElementById('act').checked
         var contTablas=document.getElementById('tablas')
         if (act == true) {
             act = 'Si'
@@ -16,20 +20,24 @@ document.addEventListener("DOMContentLoaded", function () {
             act = 'No'
         }
         var tarje = new Tarjeta(num, cvv, act);
+        tarjeta.push(tarje)
         cuenta.tarjetas.push(tarje);
         console.log(tarje)
-        contTablas.innerHTML=""
         var tabla="<table>"
-        tabla+="<tr>"
-        tabla+="<td>"
-        tabla+=cuenta.iban
-        tabla+=cuenta.tarjetas.num
-        tabla+=cuenta.tarjetas.act 
-        tabla+="</td>"
-        tabla+="</tr>"
+        tabla+="<br>"
+        for (let i = 0; i < tarjeta.length; i++) {
+            tabla+="<tr>"
+            tabla+="<td>"
+            tabla+=cuenta.iban + " " +tarjetas[i].num+" " +tarjetas[i].act 
+            tabla+="</td>"
+            tabla+="</tr>"
+        }
+        console.log(tarjeta.length)
+        tabla+="</table>"
+        contTablas.innerHTML=tabla
     }
 
-    guardar()
+   
 })
 
 
