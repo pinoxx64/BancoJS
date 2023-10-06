fbe010185ce5d4c1daaae3de5dbda7ccfeaa718f
Raul GM <loscanos3raul@gmail.com>
2023-10-06 12:41:10 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 import * as CuentaUI from './cuentaUI.js'
+import * as TarjetaUI from './tarjetaUI.js'
 
 document.addEventListener("DOMContentLoaded", function () {
 
     CuentaUI.cargarDatosCuenta()
-
+    TarjetaUI.guardar()
    
 })
 
